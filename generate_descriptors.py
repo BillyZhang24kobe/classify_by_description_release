d81222c35577c09fc56435cd9af5aa3d82194ebb
BillyZhang24kobe <billyzhang24kobe@gmail.com>
2024-04-04 18:18:06 -0400

    regenerate geode descriptors with gpt4

--- generate_descriptors.py
+++ generate_descriptors.py
@@ -15,33 +15,62 @@ client = OpenAI(
 )
 
 
+# def generate_prompt(category_name: str):
+#     # you can replace the examples with whatever you want; these were random and worked, could be improved
+#     return f"""Q: What are useful visual features for distinguishing a lemur in a photo?
+# A: There are several useful visual features to tell there is a lemur in a photo:
+# - four-limbed primate
+# - black, grey, white, brown, or red-brown
+# - wet and hairless nose with curved nostrils
+# - long tail
+# - large eyes
+# - furry bodies
+# - clawed hands and feet
+
+# Q: What are useful visual features for distinguishing a television in a photo?
+# A: There are several useful visual features to tell there is a television in a photo:
+# - electronic device
+# - black or grey
+# - a large, rectangular screen
+# - a stand or mount to support the screen
+# - one or more speakers
+# - a power cord
+# - input ports for connecting to other devices
+# - a remote control
+
+# Q: What are useful features for distinguishing a {category_name} in a photo?
+# A: There are several useful visual features to tell there is a {category_name} in a photo:
+# -
+# """
+
 def generate_prompt(category_name: str):
     # you can replace the examples with whatever you want; these were random and worked, could be improved
-    return f"""Q: What are useful visual features for distinguishing a lemur in a photo?
-A: There are several useful visual features to tell there is a lemur in a photo:
-- four-limbed primate
-- black, grey, white, brown, or red-brown
-- wet and hairless nose with curved nostrils
-- long tail
-- large eyes
-- furry bodies
-- clawed hands and feet
+    return [
+        {"role": "system", "content": f"""Q: What are useful visual features for distinguishing a lemur in a photo?
+        A: There are several useful visual features to tell there is a lemur in a photo:
+        - four-limbed primate
+        - black, grey, white, brown, or red-brown
+        - wet and hairless nose with curved nostrils
+        - long tail
+        - large eyes
+        - furry bodies
+        - clawed hands and feet
 
-Q: What are useful visual features for distinguishing a television in a photo?
-A: There are several useful visual features to tell there is a television in a photo:
-- electronic device
-- black or grey
-- a large, rectangular screen
-- a stand or mount to support the screen
-- one or more speakers
-- a power cord
-- input ports for connecting to other devices
-- a remote control
-
-Q: What are useful features for distinguishing a {category_name} in a photo?
-A: There are several useful visual features to tell there is a {category_name} in a photo:
--
-"""
+        Q: What are useful visual features for distinguishing a television in a photo?
+        A: There are several useful visual features to tell there is a television in a photo:
+        - electronic device
+        - black or grey
+        - a large, rectangular screen
+        - a stand or mount to support the screen
+        - one or more speakers
+        - a power cord
+        - input ports for connecting to other devices
+        - a remote control \n \n Please directly output answers."""},
+        {"role": "user", "content": f"""Q: What are useful features for distinguishing a {category_name} in a photo?
+        A: There are several useful visual features to tell there is a {category_name} in a photo:
+        -
+        """}
+    ]
 
 def generate_region_prompt(category_name: str, region_name: str = 'Africa'):
     # you can replace the examples with whatever you want; these were random and worked, could be improved
@@ -102,13 +131,15 @@ def obtain_descriptors_and_save(filename, class_list):
         json.dump(descriptors, fp)
     
 
-def obtain_geode_descriptors_and_save(filename, class_list, region_name='Africa'):
+def obtain_geode_descriptors_and_save(filename, class_list, region_name=''):
     responses = []
     descriptors = {}
     descriptors_list = []
     
-    
-    prompts = [generate_region_prompt(category.replace('_', ' '), region_name) for category in class_list]
+    if not region_name:
+        prompts = [generate_prompt(category.replace('_', ' ')) for category in class_list]
+    else:
+        prompts = [generate_region_prompt(category.replace('_', ' '), region_name) for category in class_list]
     
     # for category in class_list:
     #     prompt = generate_region_prompt(category.replace('_', ' '), region_name)
@@ -158,4 +189,4 @@ def obtain_geode_descriptors_and_save(filename, class_list, region_name='Africa'
 
 df = pd.read_csv('/local2/data/xuanming/geode/index.csv', index_col=False)
 class_list = list(df['object'].unique())  # class list in Geo-DE
-obtain_geode_descriptors_and_save('descriptors_geode_westasia', class_list, 'West Asia')
+obtain_geode_descriptors_and_save('descriptors_geode', class_list)
